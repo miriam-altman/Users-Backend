efddb7c8ae1e6f1d06853f3b154e760f6c2988ba
miriam-altman <miri0556788901@gmail.com>
2025-03-23 12:05:18 +0200

    push backend- fix

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import userRoutes from './routes/userRoutes';
+import userRoutes from './src/routes/userRoutes';
 import https from 'https';
 import fs from 'fs';
 
